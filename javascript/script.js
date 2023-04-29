b7be28aa8ac268bc1ff830c7adde62194987ca6f
MohamedBadrElseedy <132099550+MohamedBadrElseedy@users.noreply.github.com>
2023-04-29 23:20:24 +0200

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,37 @@
+var cart=function (product)
+{
+    switch(product)
+    {
+        case "product1":
+            alert("the product:Under Armour Men's Sportstyle Left Chest Short-sleeve T-shirt with price:25.00$ is added to cart");
+            break;
+         case "product2":
+            alert("the product:Gymreapers Weightlifting Wrist Wraps with price:19.99$ is added to cart");
+            break;
+          case "product3":
+            alert("the product:OPTIMUM NUTRITION GOLD STANDARD 100% WHEY PROTEIN 73 servings with price:85.99$ is added to cart");
+            break;
+          case "product4":
+            alert("the product:CAP 150 LB Dumbbell Set with Rack with price:189.99$ is added to cart");
+            break;
+          case "product5":
+            alert("the product:Skechers Men's GOrun Consistent-Athletic Workout Running Walking Shoe Sneaker with Air Cooled Foam with price:41.00$ is added to cart");
+            break;
+          case "product6":
+            alert("the product:ihuan Ventilated Weight Lifting Gym Workout Gloves with Wrist Wrap Support with price:19.99$ is added to cart");
+            break;
+          case "product7":
+            alert("the product:Multi Adjustable Weight Bench For Gym with price:54.50$ is added to cart");
+            break;
+          case "product8":
+            alert("the product:7mm Leather Weight Lifting Belt with price:59.99$ is added to cart");
+            break;
+          case "product9":
+            alert("the product:Under Armour Men's Tech 2.0 1/2 Zip with price:26.19$ is added to cart");
+            break;
+          case "product10":
+            alert("the product:SERIOUS MASS For Weight Gain 16 servings with price:83.99$ is added to cart");
+            break;
+        
+    }
+}
